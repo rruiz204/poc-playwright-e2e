73113c2cb54d16eb3cc219b6c8f08ce73fa3fe78
roney ruiz <roneyruizrojas@gmail.com>
2025-06-15 22:08:28 -0600

    refac(server): replace uow by prisma client

--- server/src/Infrastructure/Containers/Layers/Infrastructure.ts
+++ server/src/Infrastructure/Containers/Layers/Infrastructure.ts
@@ -1,8 +1,6 @@
 import { ContainerModule } from "inversify";
-
-import { Context } from "@Database/Common/Context";
-import { UnitOfWork } from "@Database/Common/UnitOfWork";
+import { PrismaClient } from "generated/prisma";
 
 export const Infrastructure = new ContainerModule((opts) => {
-  opts.bind(UnitOfWork).toDynamicValue(() => new UnitOfWork(Context)).inSingletonScope();
+  opts.bind(PrismaClient).toConstantValue(new PrismaClient());
 });
